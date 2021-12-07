04fb53ff78a2a466a47fa8ed0745bcc474bf8403
Mikko Syrjälä <mijsyr@utu.fi>
2021-12-07 20:56:01 +0200

    9.1. - 9.3. tehty

---
+++ PlaceholderName/bmiCalculator.ts
@@ -0,0 +1,55 @@
+interface Values {
+    height: number;
+    weight: number;
+  }
+
+const parseArguments = (args: Array<string>): Values => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+    if (height === 0 || weight === 0) throw new Error('Height and weight cannot be value 0');
+    
+    const bmi = (weight / height ** 2) * 10000;
+    
+    if (bmi < 16) {
+        return 'Underweight (severe thinness)';
+    } else if (bmi >= 16 && bmi < 17) {
+        return 'Underweight (moderate thinness)';
+    } else if (bmi >= 17 && bmi < 18.5) {
+        return 'Underweight (mild thinness)';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)';
+    } else if (bmi >= 25 && bmi < 30) {
+        return 'Overweight (pre-obese)';
+    } else if (bmi >= 30 && bmi < 35) {
+        return 'Obese (class I)';
+    } else if (bmi >= 35 && bmi < 40) {
+        return 'Obese (class II)';
+    } else if (bmi >= 40) {
+        return 'Obese (class III)';
+    } else {
+        throw new Error();
+    }
+} 
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
---
+++ PlaceholderName/exerciseCalculator.ts
@@ -0,0 +1,84 @@
+interface trainingData {
+    trainingDays: Array<number>;
+    target: number;
+  }
+
+const parseArgumentsV2 = (args: Array<string>): trainingData => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (isNaN(Number(args[2]))) throw new Error('Provided values were not numbers!');
+
+    let trainingDays: Array<number> = []; 
+    for (let i: number = 0; i < args.length - 3; i++) {
+        if (!isNaN(Number(args[3 + i]))) {
+            trainingDays[i] = Number(args[3 + i])
+        } else {
+            throw new Error('Provided values were not numbers!');
+      }
+    }
+
+    return {
+        target: Number(args[2]),
+        trainingDays: trainingDays
+    }
+}
+
+interface calculationResult {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+const rate = (target: number, average: number): number  => {
+    if (target / average < 1) {
+        return 3;
+    } else if (target / average > 1 && target / average <= 2) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+const giveRatingDescription = (rating: number): string => {
+    if (rating === 3) {
+        return 'you are godly';
+    } else if (rating === 2) {
+        return 'not too bad but could be better';
+    } else {
+        return 'you suck, better luck next time';
+    }
+}
+
+const calculateExercises = (hours: Array<number>, target: number): calculationResult => {
+    const periodLength = hours.length;
+    const trainingDays = hours.filter(h => h > 0).length;
+    const average = hours.reduce((sum: number, hours: number) => sum + hours, 0) / periodLength;
+    const dailyTarget = target;
+    const success = average >= target ? true : false;
+    const rating = rate(target, average);
+    const ratingDescription = giveRatingDescription(rating)
+
+    return {
+        periodLength: periodLength,
+        trainingDays: trainingDays,
+        success: success,
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: dailyTarget,
+        average: average
+    }
+}
+
+try {
+    const { trainingDays, target } = parseArgumentsV2(process.argv);
+    console.log(calculateExercises(trainingDays, target))
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
